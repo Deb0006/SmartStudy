40542571d725e92b0a8a6a92d7c9b276b5f65867
Debby <34969423+Deb0006@users.noreply.github.com>
2022-12-22 22:42:21 -0500

    Added description to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,28 @@
 function About() {
-  return <div>About Page</div>;
+  return (
+    <div>
+      <h1>About SmartStudy</h1>
+      <p>
+        Welcome to my study website! This platform is designed to help you learn
+        effectively and efficiently by using flashcards and a unique spaced
+        repetition system.
+        {/* With this flashcard system, you can create decks of
+        cards with questions and answers on various topics and use them to test
+        your knowledge (upcoming feature) */}
+      </p>
+      <p>
+        As you go through the cards, the algorithm will track which ones you
+        know and which ones you need more practice on. The questions that you
+        know are skipped, while the ones that you are still struggling with are
+        repeated until you feel confident in your knowledge.
+      </p>
+      <p>
+        This platform also includes a variety of customization options, such as
+        the ability to save your favorite questions, and review progress at the
+        end. With these features, you can tailor your learning experience to fit
+        your personal needs and goals.
+      </p>
+    </div>
+  );
 }
 export default About;
