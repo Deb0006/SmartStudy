5c50a0cfa59951e95708ab4be77c0c9e2cecd5fe
Debby <34969423+Deb0006@users.noreply.github.com>
2023-01-03 23:32:30 -0500

    Modified Stats table and added Favorites to it

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,6 +4,26 @@ function Quiz(props) {
   const totalQuestions = props.stats.totalQuestions;
   const value = props.value;
   const attempts = props.stats.attempts;
+
+  const table = attempts.map((question, i) => (
+    <div key={question.id}>
+      <div>
+        <div className={styles.containerStatsBottom}>
+          <p>{i + 1}</p>
+          <p>{question.question.question}</p>
+          <p>{question.attempt}</p>
+          {question.favorites ? <p>⭐</p> : <p>☆</p>}
+        </div>
+      </div>
+      <hr
+        style={{
+          color: "#d4d4d4",
+          backgroundColor: "#d4d4d4",
+        }}
+      />
+    </div>
+  ));
+
   return (
     <div className={styles.wrapper}>
       <div>
@@ -12,7 +32,7 @@ function Quiz(props) {
           <>
             <div className={styles.containerTop2}>
               <h2>Finished Session </h2>
-              <p>⭐ Favorites: {props.stats.favorites.length}</p>
+              <p>⭐ Favorites: {props.stats.totalFavorites.length}</p>
               <p>🔥 Longest Streak: {props.stats.longestStreak}</p>
               <p>⏰ {totalQuestions} Questions completed</p>
             </div>
@@ -20,7 +40,7 @@ function Quiz(props) {
         ) : (
           <div className={styles.containerTop}>
             <h2>Current Session </h2>
-            <p>⭐ {props.stats.favorites.length}</p>
+            <p>⭐ {props.stats.totalFavorites.length}</p>
             <p>🔥 {props.stats.streak}</p>
             <p>⏰ {totalQuestions} Questions</p>
           </div>
@@ -30,13 +50,17 @@ function Quiz(props) {
         <label htmlFor="quiz">{(value / totalQuestions) * 100}%</label>
         <progress id="quiz" value={value} max={totalQuestions}></progress>
       </div>
+
       {(value / totalQuestions) * 100 === 100 ? (
-        attempts.map((question) => (
-          <div key={question.id}>
-            <h3>Question #{question.index + 1}</h3>
-            <p>Number of attempts: {question.attempt}</p>
+        <div className={styles.containerStats}>
+          <div className={styles.containerStatsTop}>
+            <h4>Number</h4>
+            <h4>Question</h4>
+            <h4>Attempts</h4>
+            <h4>Favorites</h4>
           </div>
-        ))
+          {table}
+        </div>
       ) : (
         <></>
       )}
